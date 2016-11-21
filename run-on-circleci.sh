4ae02786026540f2c8e3f349d6a0ac4278e34cf2
belibak <spam1box@tut.by>
2016-11-21 11:37:02 +0100

    some commit

--- run-on-circleci.sh
+++ run-on-circleci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #git clone https://github.com/broadlook-technologies/Data_Optimization_Prototype.git
 cd sources
-git clone git@github.com:broadlook-technologies/Data_Optimization_Prototype.git
+#git clone git@github.com:broadlook-technologies/Data_Optimization_Prototype.git
+git clone https://github.com/broadlook-technologies/Data_Optimization_Prototype.git
 cd ..
-docker build -t web .
+#docker build -t web .
