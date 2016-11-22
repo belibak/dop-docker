f8bf9ce7410586b94c6e9cdd97ee581b08594d2e
belibak <spam1box@tut.by>
2016-11-22 12:31:22 +0100

    try circle 2

--- run-on-circleci.sh
+++ run-on-circleci.sh
@@ -2,4 +2,7 @@
 cd sources
 git clone git@github.com:broadlook-technologies/Data_Optimization_Prototype.git
 cd ..
-docker-compose build 
+docker build -f Dockerfile-web .
+docker build -f Dockerfile-celery .
+docker build -f Dockerfile-websockets-cleared .
+ 
