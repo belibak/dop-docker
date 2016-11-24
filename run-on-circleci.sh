ded447ab63457dedb5d543a3b79978553ad8ecae
belibak <rusetskialexander@gmail.com>
2016-11-24 15:45:18 +0100

    fix circleCi

--- run-on-circleci.sh
+++ run-on-circleci.sh
@@ -4,5 +4,5 @@ git clone git@github.com:broadlook-technologies/Data_Optimization_Prototype.git
 cd ..
 docker build -f Dockerfile-web .
 docker build -f Dockerfile-celery .
-docker build -f Dockerfile-websockets-cleared .
+docker build -f Dockerfile-websockets .
  
