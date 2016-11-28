e9e804f8d6eca54580a863b44feb5f4c887463df
belibak <spam1box@tut.by>
2016-11-28 09:14:15 +0100

    add base image

--- run-on-circleci.sh
+++ run-on-circleci.sh
@@ -2,6 +2,8 @@
 cd sources
 git clone git@github.com:broadlook-technologies/Data_Optimization_Prototype.git
 cd ..
+docker build -t dop-base -f Dockerfile-base .
+
 docker build -f Dockerfile-web .
 docker build -f Dockerfile-celery .
 docker build -f Dockerfile-websockets .
