9b8c4a6723137b2be962da2b706a6ae05a091948
belibak <spam1box@tut.by>
2016-12-02 10:28:21 +0100

    -m

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker build -t ringleaddev/base -f Dockerfile-base .
+docker push ringleaddev/base
 docker-compose build 
 
---
+++ clone-dop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone https://github.com/broadlook-technologies/Data_Optimization_Prototype.git sources/Data_Optimization_Prototype
+
